4a3afbb7ab21bc180cd4788cfc7df25f289e424f
Pedro Gomes <pdcgomes@Pedro-Gomess-iMac.local>
2012-01-04 15:25:08 +0000

    Initial Commit

---
+++ Jonez/AppDelegate.h
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.h
+//  Jonez
+//
+//  Created by Pedro Gomes on 1/4/12.
+//  Copyright __MyCompanyName__ 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class RootViewController;
+
+@interface AppDelegate : NSObject <UIApplicationDelegate> {
+	UIWindow			*window;
+	RootViewController	*viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+
+@end
---
+++ Jonez/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  Jonez
+//
+//  Created by Pedro Gomes on 1/4/12.
+//  Copyright __MyCompanyName__ 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
